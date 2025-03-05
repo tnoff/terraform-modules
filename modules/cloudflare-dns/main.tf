34fc6f7ad3719e069eea75771c24702608aa51bf
Tyler North <me@tyler-north.com>
2025-03-04 21:20:50 -0800

    chore: bump cloudflare to version 5

--- modules/cloudflare-dns/main.tf
+++ modules/cloudflare-dns/main.tf
@@ -1,9 +1,11 @@
 resource "cloudflare_zone" "this" {
-  account_id = var.cloudflare_account_id
-  zone       = var.zone_name
+  account = {
+    id = var.cloudflare_account_id
+  }
+  zone = var.zone_name
 }
 
-resource "cloudflare_record" "this" {
+resource "cloudflare_dns_record" "this" {
   for_each = toset(var.ip_list)
   zone_id  = cloudflare_zone.this.id
   name     = var.zone_name
